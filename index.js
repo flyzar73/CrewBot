824fd91ef5eff96326244e74dd314efbab1a7322
flyzar73 <flyzar73@gmail.com>
2022-08-08 10:36:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const client = new Client({intents: 3276799, partials: [Partials.Message, Partia
 ['CommandUtil', 'EventUtil', 'ButtonUtil', 'ModalUtil'].forEach(handler => { require(`./utils/handlers/${handler}`)(client) })
 
 process.on('exit', code => { console.log(`Bot stopped with code: ${code}`) })
-//process.on('uncaughtException', (err, origin) => { console.log(`${'uncaughtException'.toUpperCase()}: ${err}`, `Origin: ${origin}`)})
-//process.on('unhandledRejection', (reason, promise) => { console.log(`${'unhandledRejection'.toUpperCase()}: ${reason}\n`, promise)})
+process.on('uncaughtException', (err, origin) => { console.log(`${'uncaughtException'.toUpperCase()}: ${err}`, `Origin: ${origin}`)})
+process.on('unhandledRejection', (reason, promise) => { console.log(`${'unhandledRejection'.toUpperCase()}: ${reason}\n`, promise)})
 process.on('warning', (...args) => console.log(...args))
 
 client.login(process.env.DISCORD_TOKEN)
